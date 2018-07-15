86869f905e37b206290c142ef34f9fd58615112f
SkyHawkB <danielmey626@gmail.com>
2018-07-14 20:24:58 -0400

    change loading code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ let mainWindow;
 function createWindow() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
-  mainWindow.loadFile('./src/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   mainWindow.on('closed', () => {
     mainWindow = null;
