aa7e125d50718ec2c927445381e78dfa43a76b94
SkyHawkB <danielmey626@gmail.com>
2018-08-27 09:49:04 -0400

    [editor] begin adding editor
    (I don't know how it didn't break yet.)
    (Except that tree-view folder collapsing is sorta broken.)
    (But don't tell anyone.)

--- src/renderer.js
+++ src/renderer.js
@@ -14,14 +14,26 @@ setInterval(() => {
 
 const readdir = require('./readdir-recursive.js');
 const {ipcRenderer} = require('electron');
+const fs = require('fs');
+const _path = require('path');
 let currentWorkingDirectory;
+let currentOpenEditor;
 
+function _escape(string) {
+  return string.replace('<', '%lt%').replace('>', '%gt%');
+}
+function getPathFromElement(element) {
+  const content = element.innerHTML;
+  const path = /<span class="_meta">(.+)<\/span>/.exec(content)[1];
+
+  return _path.join(currentWorkingDirectory, path);
+}
 function render() {
   const files = readdir(currentWorkingDirectory);
   files.forEach((file) => {
     try {
       if(!/[\\\/]/g.test(file)) {
-        document.getElementById('tvl-tree-view').innerHTML += `<li><i class="far fa-file"></i> ${file}</li>`;
+        document.getElementById('tvl-tree-view').innerHTML += `<li class="tv-fi"><span class="_meta">${_escape(file)}</span><i class="far fa-file"></i> ${file}</li>`;
       } else {
         let levels = file.split(/[\\\/]/g);
         levels.splice(0, 0, 'tree-view');
@@ -29,7 +41,7 @@ function render() {
 
         for(let i in levels) {
           if(i == levels.length - 1) {
-            document.getElementById(`tvl-${levels[i - 1]}`).innerHTML += `<li><i class="far fa-file"></i> ${levels[i]}`;
+            document.getElementById(`tvl-${levels[i - 1]}`).innerHTML += `<li class="tv-fi"><span class="_meta">${_escape(file)}</span><i class="far fa-file"></i> ${levels[i]}</li>`;
           } else {
             if(!document.getElementById('tvl-' + levels[i])) {
               document.getElementById('tvl-' + levels[i - 1]).innerHTML += `<li class="tv-f tv-f-open"><i class="far fa-folder-open"></i><i class="far fa-folder"></i> ${levels[i]}<ul id="${"tvl-" + levels[i]}" class="tree-view"></ul></li>`;
@@ -42,13 +54,25 @@ function render() {
     }
   });
 
-  const elements = document.getElementsByClassName('tv-f');
-  for(let i = 0; i < elements.length; i++) {
-    let element = elements[i];
+  const folderElements = document.getElementsByClassName('tv-f');
+  for(let i = 0; i < folderElements.length; i++) {
+    let folderElement = folderElements[i];
 
-    element.addEventListener('click', () => {
-      element.classList.toggle('tv-f-open');
-      element.classList.toggle('tv-f-closed');
+    folderElement.addEventListener('click', () => {
+      folderElement.classList.toggle('tv-f-open');
+      folderElement.classList.toggle('tv-f-closed');
+    });
+  }
+
+  const fileElements = document.getElementsByClassName('tv-fi');
+  for(let i = 0; i < fileElements.length; i++) {
+    let fileElement = fileElements[i];
+
+    fileElement.addEventListener('click', () => {
+      document.getElementById('editor').innerHTML = `<textarea id="mainEditor"></textarea>`;
+
+      const path = getPathFromElement(fileElement);
+      document.getElementById('mainEditor').innerHTML = fs.readFileSync(path);
     });
   }
 };
