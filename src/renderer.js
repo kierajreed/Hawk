785df68f5cb1cb81f6129cdb2d7cdf41b9321463
SkyHawkB <danielmey626@gmail.com>
2018-08-30 23:31:39 -0400

    [misc] clean up the code so it's all nice and neat

--- src/renderer.js
+++ src/renderer.js
@@ -1,9 +1,7 @@
 function updateClock() {
   const now = new Date();
   const time = `${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}`;
-  try {
-    document.getElementById('clock').innerHTML = time;
-  } catch(e) {}
+  document.getElementById('clock').innerHTML = time;
 }
 
 updateClock();
@@ -18,7 +16,6 @@ const fs = require('fs');
 const _path = require('path');
 let currentWorkingDirectory;
 let currentOpenPath;
-let currentOpenEditor;
 
 function _escape(string) {
   return string.replace('<', '%lt%').replace('>', '%gt%');
@@ -32,26 +29,21 @@ function getPathFromElement(element) {
 function render() {
   const files = readdir(currentWorkingDirectory);
   files.forEach((file) => {
-    try {
-      if(!/[\\\/]/g.test(file)) {
-        document.getElementById('tvl-tree-view').innerHTML += `<li class="tv-fi"><span class="_meta">${_escape(file)}</span><i class="far fa-file"></i> ${file}</li>`;
-      } else {
-        let levels = file.split(/[\\\/]/g);
-        levels.splice(0, 0, 'tree-view');
-        console.log(levels);
+    if(!/[\\/]/g.test(file)) {
+      document.getElementById('tvl-tree-view').innerHTML += `<li class="tv-fi"><span class="_meta">${_escape(file)}</span><i class="far fa-file"></i> ${file}</li>`;
+    } else {
+      let levels = file.split(/[\\/]/g);
+      levels.splice(0, 0, 'tree-view');
 
-        for(let i in levels) {
-          if(i == levels.length - 1) {
-            document.getElementById(`tvl-${levels[i - 1]}`).innerHTML += `<li class="tv-fi"><span class="_meta">${_escape(file)}</span><i class="far fa-file"></i> ${levels[i]}</li>`;
-          } else {
-            if(!document.getElementById('tvl-' + levels[i])) {
-              document.getElementById('tvl-' + levels[i - 1]).innerHTML += `<li class="tv-f-open"><span class="tv-f"><i class="far fa-folder-open"></i><i class="far fa-folder"></i> ${levels[i]}</span><ul id="${"tvl-" + levels[i]}" class="tree-view"></ul></li>`;
-            }
+      for(let i in levels) {
+        if(i == levels.length - 1) {
+          document.getElementById(`tvl-${levels[i - 1]}`).innerHTML += `<li class="tv-fi"><span class="_meta">${_escape(file)}</span><i class="far fa-file"></i> ${levels[i]}</li>`;
+        } else {
+          if(!document.getElementById('tvl-' + levels[i])) {
+            document.getElementById('tvl-' + levels[i - 1]).innerHTML += `<li class="tv-f-open"><span class="tv-f"><i class="far fa-folder-open"></i><i class="far fa-folder"></i> ${levels[i]}</span><ul id="${'tvl-' + levels[i]}" class="tree-view"></ul></li>`;
           }
         }
       }
-    } catch(e) {
-      console.error(e);
     }
   });
 
@@ -70,28 +62,27 @@ function render() {
     let fileElement = fileElements[i];
 
     fileElement.addEventListener('click', () => {
-      document.getElementById('editor').innerHTML = `<textarea id="mainEditor"></textarea>`;
+      document.getElementById('editor').innerHTML = '<textarea id="mainEditor"></textarea>';
 
       const path = getPathFromElement(fileElement);
       currentOpenPath = path;
       document.getElementById('mainEditor').innerHTML = fs.readFileSync(path);
     });
   }
-};
+}
 
 ipcRenderer.on('cwdUpdate', (event, data) => {
   currentWorkingDirectory = data.cwd;
   render();
 });
 
+// eslint-disable-next-line no-unused-vars
 ipcRenderer.on('saveFile', (event, data) => {
   const content = document.getElementById('mainEditor').value;
-
-  console.log(content);
-
   fs.writeFileSync(currentOpenPath, content);
 });
 
+// eslint-disable-next-line no-unused-vars
 ipcRenderer.on('tvToggle', (event, data) => {
   let lpc = document.getElementById('left-pane').classList;
   lpc.toggle('hidden');
@@ -107,6 +98,6 @@ ipcRenderer.on('tvToggle', (event, data) => {
 });
 
 ipcRenderer.on('openFileEditor', (event, data) => {
-  document.getElementById('editor').innerHTML = `<textarea id="mainEditor"></textarea>`;
+  document.getElementById('editor').innerHTML = '<textarea id="mainEditor"></textarea>';
   document.getElementById('mainEditor').innerHTML = fs.readFileSync(data.path);
 });
